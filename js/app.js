897a1eb86a55a36d218618cecd7b5a43916c8bc2
Selbin_C_R <selbinzr@gmail.com>
2020-01-09 08:59:17 +0530

    completed list part

--- js/app.js
+++ js/app.js
@@ -1,7 +1,76 @@
 const addList = document.getElementById('add-list')
-const btngrp = document.getElementById('btn-grp')
-const allList = document.getElementsByClassName('card-items')
+const mainContainer = document.getElementById('app-main-container')
 
+let listIds = localStorage.getItem('listIds')
+  ? JSON.parse(localStorage.getItem('listIds'))
+  : []
+
+function showAllList () {
+  for (const listId of listIds) {
+    renderList(JSON.parse(localStorage.getItem(listId)))
+  }
+}
+function deleteList (event) {
+  document.getElementById('lc' + event.target.id.slice(2)).remove()
+  localStorage.removeItem(event.target.id.slice(2))
+  listIds.splice(listIds.indexOf(parseInt(event.target.id.slice(2))), 1)
+  localStorage.setItem('listIds', JSON.stringify(listIds))
+}
+function editList (event) {
+  const listName = prompt(
+    'Enter new name',
+    document.getElementById(`hd${event.target.id.slice(2)}`).innerText
+  )
+  if (listName !== '') {
+    document.getElementById(`hd${event.target.id.slice(2)}`).innerText = listName
+    const newName = JSON.parse(localStorage.getItem(event.target.id.slice(2)))
+    newName.name = listName
+    localStorage.setItem(event.target.id.slice(2), JSON.stringify(newName))
+  } else {
+    alert("name can't be empty")
+  }
+}
+
+showAllList()
+
+function renderList (list) {
+  mainContainer.appendChild(
+    createElement(
+      'div',
+      {
+        id: `lc${list.id}`,
+        className: 'list-container'
+      },
+      createElement(
+        'div',
+        {
+          id: `lh${list.id}`,
+          className: 'list-header'
+        },
+        createElement('h3', { id: `hd${list.id}` }, list.name),
+        createElement(
+          'div',
+          {
+            className: 'list-operations'
+          },
+          createElement('i', {
+            id: `ed${list.id}`,
+            className: 'far fa-edit',
+            onclick: editList
+          }),
+          createElement('i', {
+            id: `dl${list.id}`,
+            className: 'fas fa-trash-alt',
+            onclick: deleteList
+          })
+        )
+      ),
+      createElement('div', { className: 'list-todos' })
+    )
+  )
+}
+
+// function to create elements
 function createElement (type, props, ...children) {
   const dom = document.createElement(type)
   if (props) Object.assign(dom, props)
@@ -11,37 +80,18 @@ function createElement (type, props, ...children) {
   }
   return dom
 }
+
 addList.addEventListener('click', event => {
   const listName = prompt('Enter List name')
   if (listName !== '') {
-    const container = document.getElementsByClassName('list-container')
-    container[0].appendChild(
-      createElement(
-        'div',
-        { className: 'card-items' },
-        listName,
-        createElement('i', {
-          id: 1,
-          className: 'fas fa-trash-alt right',
-          onclick: event => {
-            event.target.parentNode.parentNode.removeChild(
-              event.target.parentNode
-            )
-          }
-        })
-      )
-    )
-    
-    return
+    const newList = {
+      id: listIds.length === 0 ? 1 : listIds[listIds.length - 1] + 1,
+      name: listName,
+      todo: []
+    }
+    listIds.push(newList.id)
+    localStorage.setItem('listIds', JSON.stringify(listIds))
+    localStorage.setItem(newList.id, JSON.stringify(newList))
+    renderList(newList)
   }
-  alert("List name shoudn't be empty!!")
-})
-
-/* allList.addEventListener('dblclick',event => {
-  alert("hi")
-}) */
-
-btngrp.addEventListener('click', event => {
-  console.log(event.target)
-  event.target.className = 'btn-grp'
 })
